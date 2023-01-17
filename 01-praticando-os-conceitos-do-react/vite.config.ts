be4d91922cef864de404553401d6964c0a2ad1d9
Geovane Borba <geovanne@live.com>
2023-01-17 15:30:27 -0300

    chore: remove github pages

--- 01-praticando-os-conceitos-do-react/vite.config.ts
+++ 01-praticando-os-conceitos-do-react/vite.config.ts
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://geovaneborba.github.io/rocketseat-ignite-desafios/01-praticando-os-conceitos-do-react/",
 });
