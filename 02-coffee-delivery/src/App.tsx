6d98a07a5507fd00d2a23d43e884b920f2027975
Geovane Borba <geovanne@live.com>
2023-02-06 04:11:44 -0300

    feat: add Routes

--- 02-coffee-delivery/src/App.tsx
+++ 02-coffee-delivery/src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { CartProvider } from './context/CartContext'
+import { Router } from './routes/Router'
+import { GlobalStyles } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
-  return <div className="App">Initial setup</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CartProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </CartProvider>
+
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
