75d3b2cc1d792b26899d7c72ce34155dc6568643
Geovane Borba <geovanne@live.com>
2023-02-23 17:13:07 -0300

    feat: add DefaultLayout, global styles and fonts

---
+++ 03-github-blog/src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Nunito', sans-serif;
+    line-height: 1.6;
+  }
+`
