a5cc2a7d668ab02148412b5bbf623f8b79abdffd
Geovane Borba <geovanne@live.com>
2023-03-13 16:38:46 -0300

    refactor: add responsive breakpoint in header styles

--- 03-github-blog/src/components/Header/styles.ts
+++ 03-github-blog/src/components/Header/styles.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  img {
+    width: 100%;
+  }
+
+  @media ((min-width: 320px) and (max-width: 1024px)) {
+    height: 30vh;
+
+    img {
+      object-fit: cover;
+      height: 100%;
+    }
+  }
+`
